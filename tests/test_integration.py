160eaec396b2b1d577548a87ab081b45f2e0421f
Shawn Hawken <91221452+ShawnHawken@users.noreply.github.com>
2022-11-28 17:08:19 -0800

    fix nextclade json error parsing

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -24,7 +24,23 @@ def test_jobscript_illumina(tmp_path, run_snp_mutator, run_art, run_jobscript, n
     run_snp_mutator(input_fasta_file="reference/nCoV-2019.reference.fasta", num_subs=num_subs)
     run_art(coverage=30)
     run_jobscript(input_filename="simulated_reads.fastq.gz")
-
+    
     assert os.path.exists(tmp_path / "report.pdf")
 
     shutil.copy(tmp_path / "report.pdf", f"report-illumina-{num_subs}_subs.pdf")
+
+
+def test_jobscript_illumina_nc_error(tmp_path, run_jobscript):
+    # SRA sample from CDC benchmark datasets with error in nextclade.json
+    run_jobscript(
+        input_filename="/repo/data/ARTIC/SRR16298166.fastq.gz",
+        instrument_vendor="Illumina",
+    )
+
+    assert os.path.exists(tmp_path / "report.pdf")
+
+    assert os.path.exists(tmp_path / "nextclade.json")
+
+    shutil.copy(tmp_path / "report.pdf", "report-illumina-nc_error.pdf")
+    
+    shutil.copy(tmp_path / "nextclade.json", "report-illumina-nc_error_nextclade.json")
